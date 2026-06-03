e67ebda4901fb974c7c9f567a9e3fdaa1ffb40da
Jim Phan <phanjim2@hotmail.com>
2026-06-03 02:57:21 -0700

    Implement Users Repository

---
+++ BugLogger.Infrastructure/Repositories/Users/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using BugLogger.Domain.Models;
+
+namespace BugLogger.Infrastructure.Repositories.Users
+{
+  internal interface IUsersRepository
+  {
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User?> GetById(int id);
+    Task<User?> GetByName(string name);
+  }
+}
---
+++ BugLogger.Infrastructure/Repositories/Users/UsersRepository.cs
@@ -0,0 +1,27 @@
+﻿using BugLogger.Domain.Models;
+using BugLogger.Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugLogger.Infrastructure.Repositories.Users
+{
+  internal class UsersRepository(BugLoggerDbContext context) : IUsersRepository
+  {
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+      var users = await context.Users.ToListAsync();
+      return users;
+    }
+
+    public async Task<User?> GetById(int id)
+    {
+      var user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+      return user;
+    }
+
+    public async Task<User?> GetByName(string name)
+    {
+      var user = await context.Users.FirstOrDefaultAsync(u => u.Name == name);
+      return user;
+    }
+  }
+}
